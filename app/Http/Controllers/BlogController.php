16b43f4a3d18f5aad64d709b33ef2078e4f044ad
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2021-12-03 17:11:51 +0300

    # Bugs fixed

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers;
+use Log;
 
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 date_default_timezone_set('Europe/Istanbul');
 
@@ -16,33 +17,44 @@ class BlogController extends Controller
         return json_decode(file_get_contents(public_path('assets/mglUploads/blog/' . $language_ . '.json')));
     }
 
-    public function index($language_, $itemsPerPage_ = 4, $page_ = 1) {  
+    public function index($language_, $itemsPerPage_ = null, $page_ = null) {  
         $blogs = array_reverse($this->getJsonFile($language_));        
         $result = [];
         
-        $pagination = ceil(count($blogs) / $itemsPerPage_);
-        $requestPageEndPoint = $itemsPerPage_ * $page_;
-        
-        for ($i = $requestPageEndPoint - $itemsPerPage_; $i < $requestPageEndPoint; $i++) { 
-            if($i < count($blogs)) {
-                array_push($result, $blogs[$i]);  
-            }
-        }
-        
-        if($pagination >= $page_ || $pagination == 0) {
-           return response()->json([
-                'currentPage'  => (int)$page_,
-                'totalPage'    => (int)$pagination,            
-                'itemsPerPage' => (int)$itemsPerPage_,
-                'totalItem'    => (int)count($blogs), 
-                'result'       => $result
-            ], 200); 
-        } else {
+       if($itemsPerPage_ == null && $page_ == null) {
             return response()->json([
-                'message' => 'Invalid page number',
-                'result' => []
-            ], 400);
-        }  
+                'result' => $blogs,
+            ], 200);
+       } else if ($itemsPerPage_ !== null && $page_ !== null) {            
+            $pagination = ceil(count($blogs) / $itemsPerPage_);
+            $requestPageEndPoint = $itemsPerPage_ * $page_;
+            
+            for ($i = $requestPageEndPoint - $itemsPerPage_; $i < $requestPageEndPoint; $i++) { 
+                if($i < count($blogs)) {
+                    array_push($result, $blogs[$i]);  
+                }
+            }
+            
+            if($pagination >= $page_ || $pagination == 0) {
+            return response()->json([
+                    'currentPage'  => (int)$page_,
+                    'totalPage'    => (int)$pagination,            
+                    'itemsPerPage' => (int)$itemsPerPage_,
+                    'totalItem'    => (int)count($blogs), 
+                    'result'       => $result
+                ], 200); 
+            } else {
+                return response()->json([
+                    'message' => 'Invalid page number',
+                    'result' => []
+                ], 400);
+            }  
+       }
+
+       return response()->json([
+            'message' => 'Missing parameters',      
+            'result' => []
+       ], 400);
     }
 
     public function getSelectedBlog($language_, $blogId_)
@@ -58,13 +70,15 @@ class BlogController extends Controller
         }
 
         return response()->json([
-            'message' => 'Invalid id',
+            'message' => 'Invalid BlogID',
             'result' => null
         ], 400);   
     }
 
     public function add(Request $request_, $language_)
     {
+        header("Access-Control-Allow-Methods: POST");
+
         $jsonFile = $this->getJsonFile($language_);
         $fileName = $this->generateRandomString();
 
@@ -79,8 +93,10 @@ class BlogController extends Controller
         $tempArray = [
             'BLOG_SECTION_ITEMS_ID'          => count($jsonFile) > 0 ? $jsonFile[count($jsonFile) - 1]->BLOG_SECTION_ITEMS_ID + 1 : 1,
             'BLOG_SECTION_ITEMS_FOLDER_NAME' => $fileName,
-            'BLOG_SECTION_ITEMS_THUMBNAIL'   => 'http://localhost:8000/assets/mglUploads/blog/files/' . $fileName . '/' . $thumbnailName . '.' . $thumbnailExtension,
-            'BLOG_SECTION_ITEMS_PHOTO'       => 'http://localhost:8000/assets/mglUploads/blog/files/' . $fileName . '/' . $photoName . '.' . $photoExtension,
+            'BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION' => $thumbnailExtension,
+            'BLOG_SECTION_ITEMS_PHOTO_EXTENSION' => $photoExtension,
+            'BLOG_SECTION_ITEMS_THUMBNAIL'   => 'http://127.0.0.1:8000/api/assets/mglUploads/blog/files/' . $fileName . '/' . $thumbnailName . '.' . $thumbnailExtension,
+            'BLOG_SECTION_ITEMS_PHOTO'       => 'http://127.0.0.1:8000/api/assets/mglUploads/blog/files/' . $fileName . '/' . $photoName . '.' . $photoExtension,
             'BLOG_SECTION_ITEMS_TITLE'       => $request_->title,
             'BLOG_SECTION_ITEMS_SUMMARY'     => $request_->summary,
             'BLOG_SECTION_ITEMS_ARTICLE'     => $request_->article,
@@ -90,18 +106,21 @@ class BlogController extends Controller
 
         array_push($jsonFile, $tempArray);
 
-        if (file_put_contents(public_path('assets/mglUploads/blog/' . $language_ . '.json'), json_encode($jsonFile, JSON_PRETTY_PRINT)))
+        if (file_put_contents(public_path('assets/mglUploads/blog/' . $language_ . '.json'), json_encode($jsonFile, JSON_PRETTY_PRINT))) {
             return response()->json([
                 'result' => true
             ], 200);
-        else
+        } else {
             return response()->json([
                 'result' => false
             ], 500);
+        }            
     }
 
     public function delete(Request $request_, $language_)
     {
+        header("Access-Control-Allow-Methods: DELETE");
+
         $jsonFile  = $this->getJsonFile($language_);
         $blogItems = $jsonFile;
         $jsonFile = [];
@@ -109,41 +128,40 @@ class BlogController extends Controller
 
         foreach ($blogItems as $key => $value) {
             if ($request_->id != $value->BLOG_SECTION_ITEMS_ID) {
-                $findBlog = false;
                 $tempArray = [
-                    'BLOG_SECTION_ITEMS_ID'          => (int)$value->BLOG_SECTION_ITEMS_ID,
-                    'BLOG_SECTION_ITEMS_FOLDER_NAME' => $value->BLOG_SECTION_ITEMS_FOLDER_NAME,
-                    'BLOG_SECTION_ITEMS_THUMBNAIL'   => $value->BLOG_SECTION_ITEMS_THUMBNAIL,
-                    'BLOG_SECTION_ITEMS_PHOTO'       => $value->BLOG_SECTION_ITEMS_PHOTO,
-                    'BLOG_SECTION_ITEMS_TITLE'       => $value->BLOG_SECTION_ITEMS_TITLE,
-                    'BLOG_SECTION_ITEMS_SUMMARY'     => $value->BLOG_SECTION_ITEMS_SUMMARY,
-                    'BLOG_SECTION_ITEMS_ARTICLE'     => $value->BLOG_SECTION_ITEMS_ARTICLE,
-                    'BLOG_SECTION_ITEMS_DATE'        => $value->BLOG_SECTION_ITEMS_DATE,
-                    'BLOG_SECTION_ITEMS_AUTHOR'      => $value->BLOG_SECTION_ITEMS_AUTHOR
+                    'BLOG_SECTION_ITEMS_ID'                  => (int)$value->BLOG_SECTION_ITEMS_ID,
+                    'BLOG_SECTION_ITEMS_FOLDER_NAME'         => $value->BLOG_SECTION_ITEMS_FOLDER_NAME,
+                    'BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION' => $value->BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION,
+                    'BLOG_SECTION_ITEMS_PHOTO_EXTENSION'     => $value->BLOG_SECTION_ITEMS_PHOTO_EXTENSION,
+                    'BLOG_SECTION_ITEMS_THUMBNAIL'           => $value->BLOG_SECTION_ITEMS_THUMBNAIL,
+                    'BLOG_SECTION_ITEMS_PHOTO'               => $value->BLOG_SECTION_ITEMS_PHOTO,
+                    'BLOG_SECTION_ITEMS_TITLE'               => $value->BLOG_SECTION_ITEMS_TITLE,
+                    'BLOG_SECTION_ITEMS_SUMMARY'             => $value->BLOG_SECTION_ITEMS_SUMMARY,
+                    'BLOG_SECTION_ITEMS_ARTICLE'             => $value->BLOG_SECTION_ITEMS_ARTICLE,
+                    'BLOG_SECTION_ITEMS_DATE'                => $value->BLOG_SECTION_ITEMS_DATE,
+                    'BLOG_SECTION_ITEMS_AUTHOR'              => $value->BLOG_SECTION_ITEMS_AUTHOR
                 ];
 
                 array_push($jsonFile, $tempArray);
                 $tempArray = [];
             } else {
                 $findBlog = true;
-                $explodeThumbnail = explode('.', $value->BLOG_SECTION_ITEMS_THUMBNAIL);
-                $explodePhoto = explode('.', $value->BLOG_SECTION_ITEMS_PHOTO);
 
-                if (file_exists(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail.' . $explodeThumbnail[count($explodeThumbnail) - 1]))) {
-                    unlink(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail.' . $explodeThumbnail[count($explodeThumbnail) - 1]));
+                if (file_exists(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail.' . $value->BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION))) {
+                    unlink(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail.' . $value->BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION));
                 }
 
-                if (file_exists(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo.' . $explodePhoto[count($explodePhoto) - 1]))) {
-                    unlink(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo.' . $explodePhoto[count($explodePhoto) - 1]));
+                if (file_exists(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo.' . $value->BLOG_SECTION_ITEMS_PHOTO_EXTENSION))) {
+                    unlink(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo.' . $value->BLOG_SECTION_ITEMS_PHOTO_EXTENSION));
                 }
 
                 if (is_dir(public_path('assets/mglUploads/blog/files/'  . $value->BLOG_SECTION_ITEMS_FOLDER_NAME))) {
                     rmdir(public_path('assets/mglUploads/blog/files/'  . $value->BLOG_SECTION_ITEMS_FOLDER_NAME));
                 }
             }
-        }
+        }    
 
-        if($findBlog) {
+        if($findBlog == true) {
             if (file_put_contents(public_path('assets/mglUploads/blog/' . $language_ . '.json'), json_encode($jsonFile, JSON_PRETTY_PRINT))) {
                 return response()->json([
                     'result' => true
@@ -155,36 +173,41 @@ class BlogController extends Controller
             }                
         } else {
             return response()->json([
+                'message' => "Invalid BlogID",
                 'result' => false
-            ], 404);
-        }            
+            ], 400);
+        }  
     }
 
     public function update(Request $request_, $language_)
     {
+        header("Access-Control-Allow-Methods: UPDATE");
+
         $jsonFile  = $this->getJsonFile($language_);
         $blogItems = $jsonFile;
         $jsonFile = [];
+        $findBlog = false;
 
         foreach ($blogItems as $key => $value) {
             if ($request_->id != $value->BLOG_SECTION_ITEMS_ID) {
                 $tempArray = [
-                    'BLOG_SECTION_ITEMS_ID'          => (int)$value->BLOG_SECTION_ITEMS_ID,
-                    'BLOG_SECTION_ITEMS_FOLDER_NAME' => $value->BLOG_SECTION_ITEMS_FOLDER_NAME,
-                    'BLOG_SECTION_ITEMS_THUMBNAIL'   => $value->BLOG_SECTION_ITEMS_THUMBNAIL,
-                    'BLOG_SECTION_ITEMS_PHOTO'       => $value->BLOG_SECTION_ITEMS_PHOTO,
-                    'BLOG_SECTION_ITEMS_TITLE'       => $value->BLOG_SECTION_ITEMS_TITLE,
-                    'BLOG_SECTION_ITEMS_SUMMARY'     => $value->BLOG_SECTION_ITEMS_SUMMARY,
-                    'BLOG_SECTION_ITEMS_ARTICLE'     => $value->BLOG_SECTION_ITEMS_ARTICLE,
-                    'BLOG_SECTION_ITEMS_AUTHOR'      => $value->BLOG_SECTION_ITEMS_AUTHOR,
-                    'BLOG_SECTION_ITEMS_DATE'        => $value->BLOG_SECTION_ITEMS_DATE
+                    'BLOG_SECTION_ITEMS_ID'                  => (int)$value->BLOG_SECTION_ITEMS_ID,
+                    'BLOG_SECTION_ITEMS_FOLDER_NAME'         => $value->BLOG_SECTION_ITEMS_FOLDER_NAME,
+                    'BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION' => $value->BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION,
+                    'BLOG_SECTION_ITEMS_PHOTO_EXTENSION'     => $value->BLOG_SECTION_ITEMS_PHOTO_EXTENSION,
+                    'BLOG_SECTION_ITEMS_THUMBNAIL'           => $value->BLOG_SECTION_ITEMS_THUMBNAIL,
+                    'BLOG_SECTION_ITEMS_PHOTO'               => $value->BLOG_SECTION_ITEMS_PHOTO,
+                    'BLOG_SECTION_ITEMS_TITLE'               => $value->BLOG_SECTION_ITEMS_TITLE,
+                    'BLOG_SECTION_ITEMS_SUMMARY'             => $value->BLOG_SECTION_ITEMS_SUMMARY,
+                    'BLOG_SECTION_ITEMS_ARTICLE'             => $value->BLOG_SECTION_ITEMS_ARTICLE,
+                    'BLOG_SECTION_ITEMS_DATE'                => $value->BLOG_SECTION_ITEMS_DATE,
+                    'BLOG_SECTION_ITEMS_AUTHOR'              => $value->BLOG_SECTION_ITEMS_AUTHOR
                 ];
             } else {
+                $findBlog = true;
                 if ($request_->thumbnail) {
-                    $explodeThumbnail = explode('.', $value->BLOG_SECTION_ITEMS_THUMBNAIL);
-
-                    if (file_exists(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail.' . $explodeThumbnail[count($explodeThumbnail) - 1]))) {
-                        unlink(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail.' . $explodeThumbnail[count($explodeThumbnail) - 1]));
+                    if (file_exists(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail.' . $value->BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION))) {
+                        unlink(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail.' . $value->BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION));
                     }
 
                     $thumbnailName      = $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_thumbnail';
@@ -193,10 +216,8 @@ class BlogController extends Controller
                 }
 
                 if ($request_->photo) {
-                    $explodePhoto = explode('.', $value->BLOG_SECTION_ITEMS_PHOTO);
-
-                    if (file_exists(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo.' . $explodePhoto[count($explodePhoto) - 1]))) {
-                        unlink(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo.' . $explodePhoto[count($explodePhoto) - 1]));
+                    if (file_exists(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo.' . $value->BLOG_SECTION_ITEMS_PHOTO_EXTENSION))) {
+                        unlink(public_path('assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo.' . $value->BLOG_SECTION_ITEMS_PHOTO_EXTENSION));
                     }
 
                     $photoName      = $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '_photo';
@@ -205,15 +226,17 @@ class BlogController extends Controller
                 }
 
                 $tempArray = [
-                    'BLOG_SECTION_ITEMS_ID'          => (int)$value->BLOG_SECTION_ITEMS_ID,
-                    'BLOG_SECTION_ITEMS_FOLDER_NAME' => $value->BLOG_SECTION_ITEMS_FOLDER_NAME,
-                    'BLOG_SECTION_ITEMS_THUMBNAIL'   => $request_->thumbnail ? 'http://localhost:8000/assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $thumbnailName . '.' . $thumbnailExtension : $value->BLOG_SECTION_ITEMS_THUMBNAIL,
-                    'BLOG_SECTION_ITEMS_PHOTO'       => $request_->photo     ? 'http://localhost:8000/assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $photoName . '.' . $photoExtension         : $value->BLOG_SECTION_ITEMS_PHOTO,
-                    'BLOG_SECTION_ITEMS_TITLE'       => $request_->title,
-                    'BLOG_SECTION_ITEMS_SUMMARY'     => $request_->summary,
-                    'BLOG_SECTION_ITEMS_ARTICLE'     => $request_->article,
-                    'BLOG_SECTION_ITEMS_AUTHOR'      => $request_->author,
-                    'BLOG_SECTION_ITEMS_DATE'        => $value->BLOG_SECTION_ITEMS_DATE,
+                    'BLOG_SECTION_ITEMS_ID'                  => (int)$value->BLOG_SECTION_ITEMS_ID,
+                    'BLOG_SECTION_ITEMS_FOLDER_NAME'         => $value->BLOG_SECTION_ITEMS_FOLDER_NAME,
+                    'BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION' => $request_->thumbnail ? $thumbnailExtension : $value->BLOG_SECTION_ITEMS_THUMBNAIL_EXTENSION,
+                    'BLOG_SECTION_ITEMS_PHOTO_EXTENSION'     => $request_->photo ? $photoExtension : $value->BLOG_SECTION_ITEMS_PHOTO_EXTENSION,
+                    'BLOG_SECTION_ITEMS_THUMBNAIL'           => $request_->thumbnail ? 'http://127.0.0.1:8000/api/assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $thumbnailName . '.' . $thumbnailExtension : $value->BLOG_SECTION_ITEMS_THUMBNAIL,
+                    'BLOG_SECTION_ITEMS_PHOTO'               => $request_->photo     ? 'http://127.0.0.1:8000/api/assets/mglUploads/blog/files/' . $value->BLOG_SECTION_ITEMS_FOLDER_NAME . '/' . $photoName . '.' . $photoExtension         : $value->BLOG_SECTION_ITEMS_PHOTO,
+                    'BLOG_SECTION_ITEMS_TITLE'               => $request_->title,
+                    'BLOG_SECTION_ITEMS_SUMMARY'             => $request_->summary,
+                    'BLOG_SECTION_ITEMS_ARTICLE'             => $request_->article,
+                    'BLOG_SECTION_ITEMS_AUTHOR'              => $request_->author,
+                    'BLOG_SECTION_ITEMS_DATE'                => $value->BLOG_SECTION_ITEMS_DATE,
                 ];
             }
 
@@ -221,13 +244,21 @@ class BlogController extends Controller
             $tempArray = [];
         }
 
-        if (file_put_contents(public_path('assets/mglUploads/blog/' . $language_ . '.json'), json_encode($jsonFile, JSON_PRETTY_PRINT)))
+        if($findBlog == true) {
+            if (file_put_contents(public_path('assets/mglUploads/blog/' . $language_ . '.json'), json_encode($jsonFile, JSON_PRETTY_PRINT))) {
+                return response()->json([
+                    'result' => true
+                ], 200);
+            } else {
+                return response()->json([
+                    'result' => false
+                ], 500);
+            }         
+        } else {            
             return response()->json([
-                'result' => true
-            ], 200);
-        else
-            return response()->json([
+                'message' => 'Invalid blog id',
                 'result' => false
-            ], 500);
+            ], 400);
+        }            
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,15 @@ use App\Http\Controllers\GalleryController;
 |
 */
 
-Route::post('/mglBlog/add/{language}', [BlogController::class, 'add']);
-Route::post('/mglBlog/delete/{language}', [BlogController::class, 'delete']);
-Route::post('/mglBlog/update/{language}', [BlogController::class, 'update']);
-Route::get('/mglBlog/{language}/{itemsPerPage?}/{page?}', [BlogController::class, 'index']);
-Route::get('/mglBlog/{language}/{id}', [BlogController::class, 'getSelectedBlog']);
-Route::post('/mglCareer/add', [CareerController::class, 'add']);
-Route::get('/mglCareer', [CareerController::class, 'index']);
-Route::get('/mglHome/select', [HomeController::class, 'selectDailyPhoto']);
-Route::get('/mglHome', [HomeController::class, 'index']);
-Route::post('/mglLog', [HomeController::class, 'userLogs']);
-Route::get('/mglGetVideos/{language}', [GalleryController::class, 'index']);
+Route::post('mglBlog/add/{language}', [BlogController::class, 'add']);
+Route::post('mglBlog/delete/{language}', [BlogController::class, 'delete']);
+Route::post('mglBlog/update/{language}', [BlogController::class, 'update']);
+Route::get('mglBlog/select/{language}/{id}', [BlogController::class, 'getSelectedBlog']);
+Route::get('mglBlog/{language}/{itemsPerPage?}/{page?}', [BlogController::class, 'index']);
+Route::post('mglCareer/add', [CareerController::class, 'add']);
+Route::post('mglCareer/delete', [CareerController::class, 'delete']);
+Route::get('mglCareer', [CareerController::class, 'index']);
+Route::get('mglHome/select', [HomeController::class, 'selectDailyPhoto']);
+Route::get('mglHome', [HomeController::class, 'index']);
+Route::post('mglLog', [HomeController::class, 'userLogs']);
+Route::get('mglGallery', [GalleryController::class, 'index']);
